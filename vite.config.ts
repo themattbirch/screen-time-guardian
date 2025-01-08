e42d07e4370654d5c05eb0b1f5012409ae0d9be7
Matt B <matt@optimizewebsolutions.com>
2025-01-08 02:51:49 -0500

    Server changes

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         website: resolve(__dirname, 'index.html'),          
-        app: resolve(__dirname, 'public/index.html')        
+        app: resolve(__dirname, 'public/index.html'),
+        support: resolve(__dirname, 'public/support.html'),
+        privacy: resolve(__dirname, 'public/privacy.html'),
+        terms: resolve(__dirname, 'public/terms.html'),
+        
       },
       output: {
         entryFileNames: 'assets/[name].[hash].js',
